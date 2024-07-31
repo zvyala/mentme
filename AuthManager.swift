03af78b4d5edcccafd455519355c7c72d7fb845c
zaynah alam <149890483+zvyala@users.noreply.github.com>
2024-07-31 12:17:19 +0100

    Update AuthManager.swift

--- AuthManager.swift
+++ AuthManager.swift
@@ -9,15 +9,12 @@ import Foundation
 import FirebaseAuth
 
 class AuthManager: ObservableObject {
-    @Published var isLoggedIn = false // State variable to track login status
-
+    @Published var isLoggedIn = false 
     func signIn(email: String, password: String, completion: @escaping (Error?) -> Void) {
         Auth.auth().signIn(withEmail: email, password: password) { [weak self] authResult, error in
             if let error = error {
-                // Handle sign in error
                 completion(error)
             } else {
-                // Sign in successful
                 self?.isLoggedIn = true
                 completion(nil)
             }
